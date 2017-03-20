dcc82999b95b5810807fbb130b8ab23af63428b1
domee1337 <dominikjoedecke@outlook.de>
2017-03-20 21:23:43 +0100

    New Update

---
+++ src/Providers/WarehouseServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Warehouse\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class WarehouseServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
+
+?>
